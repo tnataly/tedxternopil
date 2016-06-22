80ab954f7945d20e3da2f8188bf44d2e17fe7608
Nataly Tkachuk <ny.tkachuk@gmail.com>
2016-06-22 15:16:49 +0300

    Add styles to partners and venue

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,38 @@
 $(function() {
 
 // Display a google map inside the venue section
+/*
   function init_map() {
     var var_location = new google.maps.LatLng(49.554557, 25.597648);
     var var_center = new google.maps.LatLng(49.554643, 25.600495);
 
     var var_mapoptions = {
       center: var_center,
-      zoom: 18
+      zoom: 18,
+      scaleControl: false,
+      streetViewControl: false,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+
     };
 
     var var_marker = new google.maps.Marker({
       position: var_location,
       map: var_map,
-      title:"Палац кіно"});
+      title:"Палац кіно",
+      draggable: false,
+      animation: google.maps.Animation.DROP
+    });
 
-    var var_map = new google.maps.Map(document.getElementById("venue"),
+    var var_map = new google.maps.Map(document.getElementById("map"),
         var_mapoptions);
 
     var_marker.setMap(var_map);
 
       }
 
-    google.maps.event.addDomListener(window, 'load', init_map);
 
+    google.maps.event.addDomListener(window, 'load', init_map);
+*/
  // Preloader or something else....
 
 
