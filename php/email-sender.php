5688f52f5236b52ce6dc24a13f076e1213ac18e2
Nataly Tkachuk <ny.tkachuk@gmail.com>
2016-06-22 17:04:23 +0300

    First production version

---
+++ php/email-sender.php
@@ -0,0 +1,43 @@
+<?php
+session_cache_limiter('nocache');
+header('Expires: ' . gmdate('r', 0));
+header('Content-type: application/json');
+
+$Recipient = 'ny.tkachuk@gmail.com'; // <-- Set your email here
+
+$subject = $_POST['subject'];
+
+if($Recipient) {
+
+	$Name = $_POST['contact-name'];
+	$Email = $_POST['contact-email'];
+	$Message = $_POST['contact-words'];
+	$Subject = "Нове повідомлення через контактну форму";
+
+	$Email_body = "";
+	$Email_body .= "From: " . $Name . "\n" .
+				   "Email: " . $Email . "\n" .
+				   "Subject: " . $Subject . "\n" .
+				   "Message: " . $Message . "\n" .
+
+	$Email_headers = "";
+	$Email_headers .= 'From: ' . $Name . ' <' . $Email . '>' . "\r\n".
+					  "Reply-To: " .  $Email . "\r\n";
+
+	$sent = mail($Recipient, $Subject, $Email_body, $Email_headers);
+
+	if ($sent){
+		$emailResult = array ('sent'=>'yes');
+	} else{
+		$emailResult = array ('sent'=>'no');
+	}
+
+	echo json_encode($emailResult);
+
+} else {
+
+	$emailResult = array ('sent'=>'no');
+	echo json_encode($emailResult);
+
+}
+?>
